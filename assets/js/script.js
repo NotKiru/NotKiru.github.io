e57f3c2563881bf385429a34ea6115c7d4f1c3b1
Kacper <kwartaofficial@gmail.com>
2023-01-06 18:45:58 +0100

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,12 @@ if (position > msg.length) position = 0
 window.setTimeout("scrolltitle()",170);
 }
 scrolltitle();
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
